bac4544c8d1af11d8e5882919833cf42dd10a913
Leonardo Scorza <leonardoscorzas88@gmail.com>
2016-10-06 17:15:08 -0300

    Fix Question controller

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,13 @@
+class QuestionsController < ApplicationController
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,8 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
 
-  # RESTful
   resource :question
-
-  # CRUD REST substituido pelo resource
-  # get 'question/index'
-  # get 'question/create'
-  # get 'question/update'
-  # get 'question/destroy'
-
 
   get 'home/index'
   get 'home/about'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
