31ba77f920cc1a607a5829d958b3eee5c1dab5c3
Leonardo Scorza <leonardoscorzas88@gmail.com>
2016-10-05 23:48:06 -0300

    Fix devise postgre type

--- db/migrate/20161005234110_add_devise_to_users.rb
+++ db/migrate/20161005234110_add_devise_to_users.rb
@@ -4,7 +4,6 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :name
 
       ## Recoverable
       t.string   :reset_password_token
@@ -17,8 +16,8 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.0]
       t.integer  :sign_in_count, default: 0, null: false
       t.datetime :current_sign_in_at
       t.datetime :last_sign_in_at
-      t.inet     :current_sign_in_ip
-      t.inet     :last_sign_in_ip
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
 
       ## Confirmable
       # t.string   :confirmation_token
--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20161005234110) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "answers", force: :cascade do |t|
     t.integer  "question_id"
     t.integer  "user_id"
@@ -55,17 +52,16 @@ ActiveRecord::Schema.define(version: 20161005234110) do
     t.datetime "updated_at",                          null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
-    t.string   "name"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
