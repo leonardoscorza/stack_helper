bd55f7ac07640116bb8eab57024bdfd7223cdca1
Samuel Rubens <samuel.rubens@live.com>
2016-10-06 16:44:33 -0300

    Add model question/Refatoring routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
-  get 'question/index'
-  get 'question/create'
-  get 'question/update'
-  get 'question/destroy'
 
-  
+  # RESTful
+  resource :question
+
+  # CRUD REST substituido pelo resource
+  # get 'question/index'
+  # get 'question/create'
+  # get 'question/update'
+  # get 'question/destroy'
+
+
   get 'home/index'
   get 'home/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
