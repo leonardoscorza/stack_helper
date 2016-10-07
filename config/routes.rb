33e52e2a55a1789f16a9429c616c1bcef00e7df5
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2016-10-07 02:25:24 +0000

    Fix question routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
 
-  resource :question
+  resources :question
 
   get 'home/index'
   get 'home/about'
