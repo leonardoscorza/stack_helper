85d63c4e1f1e659fad2d67e8b39af6f4f3fb8ff4
Elizeu Santos <elizeusdsantos@gmail.com>
2016-10-09 08:17:28 -0300

    Fix resource param.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
 
-  resources :question
+  resources :questions
 
   get 'home/index'
   get 'home/about'
