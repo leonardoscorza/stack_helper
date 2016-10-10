2a0b84e279d31076a0d247da976cf4f1ee944cbc
Leonardo Scorza <leonardoscorzas88@gmail.com>
2016-10-09 21:57:21 -0300

    fix social login

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,13 @@ class User < ApplicationRecord
   has_many :answers
   has_many :questions
   validates :name, presence: true, length: { maximum: 50 }
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+       user.email = auth.info.email
+       user.name = auth.info.name
+       user.password = Devise.friendly_token[0,20]
+       user.image = auth.info.image
+     end
+  end
 end
