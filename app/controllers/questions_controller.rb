355c539f6e1d58f405f4c041bce98c8697bec6c4
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2016-10-10 22:52:43 +0000

    Add questions crud (show, new, create, destroy, update)

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :set_question, only: [:show, :edit, :update, :destroy]
+  before_action :get_question, only: [:show, :edit, :update, :destroy]
 
   before_action :authenticate_user!, only: [:create, :update, :destroy]
 
@@ -9,23 +9,41 @@ class QuestionsController < ApplicationController
   end
 
   def create
+    @question = Question.new(question_params)
+    if @question.save
+      flash[:info] = "Questão criada!"
+      redirect_to root_url
+    else
+      render 'new'
+    end
   end
 
   def update
+    if @question.update_attributes(question_params)
+      flash[:success] = "Questão atualizada!"
+      redirect_to @question
+    else
+      # TODO
+    end
   end
 
   def destroy
+    @question.destroy
+    flash[:success] = "Questão deletada!"
+    redirect_to root_url
   end
 
   def show
+    @user = @question.user
   end
 
   private
-    def set_question
+
+    def get_question
       @question = Question.find(params[:id])
     end
 
     def question_params
-      params.require(:question).permit(:title, :text, :user_id)
+      params.require(:question).permit(:title, :text, :user_id,:tag_body)
     end
 end
