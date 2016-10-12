5e65daab47ac7ae80614961802717f2307e61eea
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2016-10-12 13:36:57 +0000

    Add new tests

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,19 +1,43 @@
 require 'rails_helper'
 
 describe QuestionsController, type: :controller do
+  describe "GET #index" do
+    before { get :index }
+
+    it "renders :index view" do
+      expect(response).to render_template(:index)
+    end
+
+    it "assigns the requested object to @questions" do
+      expect(assigns(:questions)).to eq(Question.all)
+    end
+  end
+
   describe "GET #edit" do
     let(:question) { create(:question) }
-    before do
-      sign_in create(:user)
-      get :edit, params: { id: question, question: attributes_for(:question) }
+    subject { get :edit, params: { id: question, question: attributes_for(:question) } }
+
+    context "when logged-in" do
+      before do
+        sign_in create(:user)
+        subject
+      end
+
+      it "renders :edit view" do
+        expect(response).to render_template(:edit)
+      end
+
+      it "assigns the requested object to @question" do
+        expect(assigns(:question)).to eq(question)
+      end
     end
 
-    it "render :edit view" do
-      expect(response).to render_template(:edit)
-    end
+    context "when not logged-in" do
+      before { subject }
 
-    it "assigns the requested object to @question" do
-      expect(assigns(:question)).to eq(question)
+      it "redirects to sign-in page" do
+        expect(response).to redirect_to(new_user_session_url)
+      end
     end
   end
 
@@ -21,7 +45,7 @@ describe QuestionsController, type: :controller do
     let(:question) { create(:question_with_answers) }
     before { get :show, params: { id: question } }
 
-    it "render :show view" do
+    it "renders :show view" do
       expect(response).to render_template(:show)
     end
 
@@ -32,75 +56,151 @@ describe QuestionsController, type: :controller do
   end
 
   describe "GET #new" do
-    before do
-      sign_in create(:user)
-      get :new
+    subject { get :new }
+
+    context "when logged-in" do
+      before do
+        sign_in create(:user)
+        subject
+      end
+
+      it "renders :new view" do
+        expect(response).to render_template(:new)
+      end
+
+      it "makes available an empty question object" do
+        expect(assigns(:question)).to be_a_new(Question)
+      end
     end
 
-    it "render :new view" do
-      expect(response).to render_template(:new)
-    end
+    context "when not logged-in" do
+      before { subject }
 
-    it "makes available an empty question object" do
-      expect(assigns(:question)).to be_a_new(Question)
+      it "redirects to sign-in page" do
+        expect(response).to redirect_to(new_user_session_url)
+      end
     end
   end
 
   describe "POST #create" do
-    before { sign_in create(:user) }
+    subject(:post_with_valid_attributes) { post :create,
+             params: { question: attributes_for(:question) } }
 
-    context "whit valid attributes" do
-      it "creates a new question" do
-        expect{ post :create, params:
-          { question: attributes_for(:question) } }.to change(Question, :count).by(1)
+    context "when logged-in -->" do
+      before { sign_in create(:user) }
+
+      context "whit valid attributes" do
+        it "creates a new question" do
+          expect{ post_with_valid_attributes }.to change(Question, :count).by(1)
+        end
+
+        it "associate the question to a user" do
+          post_with_valid_attributes
+          expect(assigns(:question).user_id).to_not be_nil
+        end
+
+        it "redirects to the new question" do
+          post_with_valid_attributes
+          expect(response).to redirect_to(Question.last)
+        end
       end
 
-      it "associa the question to a user" do
-        post :create, params: { question: attributes_for(:question) }
-        expect(assigns(:question).user_id).to_not be_nil
+      context "whit invalid attributes" do
+        subject(:post_with_invalid_attributes) { post :create,
+                params: { question: attributes_for(:question, :invalid) } }
+
+        it "does not save the new question" do
+          expect{ post_with_invalid_attributes }.to_not change(Question, :count)
+        end
+
+        it "renders the new template" do
+          post_with_invalid_attributes
+          expect(response).to render_template(:new)
+        end
       end
     end
 
-    context "whit invalid attributes" do
-      it "does not save the new question" do
-        expect{ post :create, params: { question:
-          attributes_for(:question, :invalid) } }.to_not change(Question, :count)
+    context "when not logged-in" do
+      before { post_with_valid_attributes }
+
+      it "redirects to sign-in page" do
+        expect(response).to redirect_to(new_user_session_url)
       end
     end
   end
 
   describe "PUT #update" do
-    before { sign_in create(:user) }
-    let(:question) { create(:question) }
+    context "when logged-in -->" do
+      before { sign_in create(:user) }
+      let(:question) { create(:question) }
 
-    context "whit valid attributes" do
-      it "find the requested question" do
-        put :update, params: { id: question, question: attributes_for(:question) }
-        expect(assigns(:question)).to eq(question)
+      context "whit valid attributes" do
+        subject { put :update, params: { id: question,
+                  question: attributes_for(:question, title: "New title") } }
+
+        it "finds the requested question" do
+          subject
+          expect(assigns(:question)).to eq(question)
+        end
+
+        it "updates question attributes" do
+          subject
+          expect(question.reload.title).to eq("New title")
+        end
+
+        it "redirects to the question" do
+          subject
+          expect(response).to redirect_to(question)
+        end
       end
 
-      it "update question attributes" do
-        put :update, params: { id: question, question: attributes_for(:question, title: "New title") }
-        expect(question.reload.title).to eq("New title")
+      context "whit invalid attributes" do
+        before { put :update, params: { id: question, question: attributes_for(:question, :invalid) } }
+
+        it "does not update question attributes" do
+          # "to_not be_nil" because invalid question factory is setting title = nil.
+          expect(question.reload.title).to_not be_nil
+        end
+
+        it "renders the new template" do
+          expect(response).to render_template(:edit)
+        end
       end
     end
 
-    context "whit invalid attributes" do
-      before { put :update, params: { id: question, question: attributes_for(:question, :invalid) } }
+    context "when not logged-in" do
+      before { put :update, params: { id: create(:question),
+                question: attributes_for(:question, title: "New title") } }
 
-      it "does not update question attributes" do
-        # "to_not be_nil" because invalid question factory is seting title = nil.
-        expect(question.reload.title).to_not be_nil
+      it "redirects to sign-in page" do
+        expect(response).to redirect_to(new_user_session_url)
       end
     end
   end
 
   describe "DELETE #destroy" do
-    before { sign_in create(:user) }
     let!(:question) { create(:question) }
+    subject { delete :destroy, params: { id: question } }
 
-    it "deletes the question" do
-      expect { delete :destroy, params: { id: question } }.to change(Question, :count).by(-1)
+    context "when logged-in" do
+      before { sign_in create(:user) }
+
+      it "deletes the question" do
+        expect { subject }.to change(Question, :count).by(-1)
+      end
+
+      it "redirects to index view" do
+        subject
+        expect(response).to redirect_to(questions_url)
+      end
+    end
+
+    context "when not logged-in" do
+      before { subject }
+
+      it "redirects to sign-in page" do
+        expect(response).to redirect_to(new_user_session_url)
+      end
     end
   end
 end
