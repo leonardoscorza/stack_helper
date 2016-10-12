b6d733d782ac13761b480e6ff4585c26f606627a
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2016-10-12 13:35:58 +0000

    Remove 'only' parameter in resources questions

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,9 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
 
-  resources :question
+  resources :questions
+
   root to: 'home#index'
-
-  resources :questions, only: [:new, :create, :destroy, :edit, :update, :show]
-
   get 'home/index'
   get 'home/about'
 end
