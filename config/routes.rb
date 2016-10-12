aadfb96f43cb04f41cce7f30045f42ef59766d7d
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2016-10-12 02:05:36 +0000

    Update to include only necessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,10 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
 
-
   resources :question
   root to: 'home#index'
 
-  resources :questions
-
+  resources :questions, only: [:new, :create, :destroy, :edit, :update, :show]
 
   get 'home/index'
   get 'home/about'
