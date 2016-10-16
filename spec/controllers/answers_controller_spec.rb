3894ade06629c878320c8fc78c14803d6cc682c1
rauanfelipe <rauan.assis@outlook.com>
2016-10-16 17:25:21 -0200

    updating issue(#34)

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -7,27 +7,7 @@ RSpec.describe AnswersController, type: :controller do
   end
 
   describe "POST #create" do
-    subject(:post_with_valid_attributes) { post :create, params: { answer: attributes_for(:answer), question_id: @question }}
 
-    context "when looged-in" do
-      before { sign_in create(:user) }
-
-      context "with valid attributes" do
-        it "create new answer" do
-          expect{ post_with_valid_attributes }.to change(Answer, :count).by(1)
-        end
-
-        it "associate the answers to a user" do
-          post_with_valid_attributes
-          expect(assigns(:answer).user_id).to_not be_nil
-        end
-
-        it "associate the answer to a question" do
-          post_with_valid_attributes
-          expect(assigns(:answer).question_id).to_not be_nil
-        end
-      end
-    end
   end
 
   describe "PUT #update" do
