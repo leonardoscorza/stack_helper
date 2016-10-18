9e7bd488407ab5675e29ed8bc541af4856f61db6
rfranca86 <universius@gmail.com>
2016-10-18 14:34:46 -0200

    configurando o paperclip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
 		def configure_permitted_parameters
 	    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
-	    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+	    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :avatar) }
 		end
 
 end
