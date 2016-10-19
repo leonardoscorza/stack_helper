5bafffbedc72b2b19d45c040fa9c9a1c0e4f35b6
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2016-10-19 23:59:45 +0000

    Generate users controller and configure title provider in show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
 
   resources :questions
   resources :answers, only: [:create]
+  resources :users, only: [:show]
 
   root to: 'home#index'
   get 'home/index'
   get 'about' => 'home#about', as: :about
+
 
   mount ActionCable.server => '/cable'
 end
