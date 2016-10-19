d6de2814c4e73a1ea8aa135e34c8db6d57e1b817
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2016-10-19 23:43:21 +0000

    Add title tag tests

---
+++ spec/features/users_show_layout_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "Home index view" do
+  describe "Page title" do
+    let(:user) { create(:user) }
+    before do
+      login_as user
+      visit users_url(user)
+    end
+
+    it "displays 'project name'" do
+      expect(page).to have_title("StackHelper")
+    end
+  end
+end
