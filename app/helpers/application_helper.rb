5bbdc9c890083d2cecc551d0206121922dc54067
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2016-10-19 23:45:49 +0000

    Add helpers to use in title tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(title)
+    full_title = "StackHelper"
+    if title.empty?
+      full_title
+    else
+      "#{title} - #{full_title}"
+    end
+  end
 end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,5 @@
 module QuestionsHelper
+  def first_tag(question)
+    Hashtag.find(@question.hashtag_ids.first).name
+  end
 end
