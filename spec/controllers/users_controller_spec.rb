d15adfcf89884141d429ff1683429aa83f574c7e
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2016-10-20 00:04:52 +0000

    Add users controller basic tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,9 +3,15 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
 
   describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+    let(:user) { create(:user) }
+    before { get :show, params: { id: user } }
+
+    it "renders :show view" do
+      expect(response).to render_template(:show)
+    end
+
+    it "assigns the requested object to @user" do
+      expect(assigns(:user)).to eq(user)
     end
   end
 
