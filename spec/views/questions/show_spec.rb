076a5c743e69eebae9f7ec84ee8c04a400b04be4
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2016-10-20 16:04:05 +0000

    Add tests for meta description

--- spec/views/questions/show_spec.rb
+++ spec/views/questions/show_spec.rb
@@ -1,16 +1,27 @@
 require 'rails_helper'
 
 describe "questions/show.html.erb", type: :view do
+  # question must be created (instead builded) because the hashtags are used
+  # in show view, and they are created only when the question is saved.
+  let(:question) { create(:question) }
   before do
-    # question must be created (instead builded) because the hashtags are used
-    # in show view, and they are created only when the question is saved.
-    assign(:question, create(:question))
+    assign(:question, question)
     assign(:user, build(:user))
     assign(:answer, build(:answer))
     render template: "questions/show", layout: "layouts/application"
   end
 
-  it "displays 'question first tag - question title - project name'" do
-    expect(rendered).to have_title("ruby - This is the question title - StackHelper")
+  describe "Title tag" do
+    it "displays 'question first tag - question title - project name'" do
+      expect(rendered).to have_title("ruby - This is the question title - StackHelper")
+    end
+  end
+
+  describe "Meta description" do
+    it "displays truncated question text" do
+      meta_description = truncate_question_text(question.text)
+      expect(rendered).to have_selector("meta[name='description'][content='#{meta_description}']",
+                                        visible: false)
+    end
   end
 end
--- spec/views/users/show_spec.rb
+++ spec/views/users/show_spec.rb
@@ -6,7 +6,17 @@ describe "users/show.html.erb", type: :view do
     render template: "users/show", layout: "layouts/application"
   end
 
-  it "displays 'user name - project name'" do
-    expect(rendered).to have_title("User - StackHelper")
+  describe "Title tag" do
+    it "displays 'user name - project name'" do
+      expect(rendered).to have_title("Usuário User - StackHelper")
+    end
+  end
+
+  describe "Meta description" do
+    it "displays users default description" do
+      meta_description = "Usuário do StackHelper."
+      expect(rendered).to have_selector("meta[name='description'][content='#{meta_description}']",
+                                        visible: false)
+    end
   end
 end
